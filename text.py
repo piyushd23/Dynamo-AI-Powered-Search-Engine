50a9756c145c7bdd3ddac6170376e17d8d144374
Piyush Deshmukh <133801778+piyushd23@users.noreply.github.com>
2024-04-19 21:55:47 +0530

    Update text.py

--- text.py
+++ text.py
@@ -13,7 +13,7 @@ def stptime():
 
 st.title("Dynamo AI ⌨")
 
-os.environ['GOOGLE_API_KEY'] = "YOUR_API_KEY_HERE"
+os.environ['GOOGLE_API_KEY'] = "YOUR_GOOGLE_API_KEY_HERE"
 genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
 
 # model is "gemini pro"
